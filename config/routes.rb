be59e56452c4d048069d16a9f5eeaf4211072fab
Kate Brown <kbrown8158@gmail.com>
2017-01-18 15:04:58 -0500

    updated before_action in routes"
    q
    !!!

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
   resources :shoes, only: [:index]
 
   root "shoes#index"
